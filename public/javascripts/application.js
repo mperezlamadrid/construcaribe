c84b764915016089d40ce694579d8737fe507099
Manuel <mperez@wawand.co>
2015-01-16 15:38:41 -0500

    I added the header

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,3 @@
 $(function(){	
-	$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-	});
 
-	$("#social-network").click(function(){
-		$("#sidebar-wrapper li.list-networks").toggleClass("hide");
-	});
 });
